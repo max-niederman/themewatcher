f26fd7d8486f8401d819dbab32a37684c1da489e
Max Niederman <max@maxniederman.com>
2021-06-19 12:21:03 -0700

    feat(native): add file watcher code

---
+++ native.ts
@@ -0,0 +1,15 @@
+const path = await Deno.realPath(Deno.args[0] ?? "theme.json");
+
+async function print() {
+  await Deno.copy(
+    await Deno.open(path),
+    Deno.stdout,
+  )
+}
+
+await print();
+
+const watcher = Deno.watchFs(path);
+for await (const event of watcher) {
+  print();
+}
